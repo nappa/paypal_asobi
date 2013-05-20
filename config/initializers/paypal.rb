99c39713b50f3b5029d5ddf7cf58a01ca7f2f621
Ryosuke Yamazaki <ryosuke.yamazaki@mac.com>
2013-05-21 06:56:08 +0900

    wip

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -16,7 +16,15 @@ PaypalConfig = (
     # TODO
     raise 'fill here'
   when 'production'
-    # TODO
-    raise 'fill here'
+    {
+      paypal_url:       'https://www.sandbox.paypal.com/cgi-bin/webscr',
+      paypal_email:     'suoaoshi@kimaroki.jp',
+      paypal_cert_id:   'YYFQ434FHXAF6',
+      path: {
+        paypal_cert:     "#{Rails.root}/config/paypal_cert_pem.txt",
+        merchant_cert:   "#{Rails.root}/config/paypal_my_public_key.pem",
+        merchant_pubkey: "#{Rails.root}/config/paypal_my_private_key.pem",
+      },
+    }
   end
 )
