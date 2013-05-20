0b8827fcfd19af38132b70dbe9917cb2c2c07558
Ryosuke Yamazaki <ryosuke.yamazaki@mac.com>
2013-05-21 06:08:27 +0900

    wip

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module PaypalAsobi
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,5 +20,7 @@ module PaypalAsobi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.initialize_on_precompile = false
   end
 end
