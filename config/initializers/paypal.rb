9bb86f0e5e5f02fd80a2fb514584b60fb581f228
Ryosuke Yamazaki <ryosuke.yamazaki@mac.com>
2013-05-25 09:08:01 +0900

    wip

--- config/initializers/paypal.rb
+++ config/initializers/paypal.rb
@@ -1,15 +1,24 @@
+# -*- coding: utf-8 -*-
 
 PaypalConfig = (
   case Rails.env
   when 'development'
     {
+      # PayPal POST Endpoint URL
       paypal_url:       'https://www.sandbox.paypal.com/cgi-bin/webscr',
+      # ビジネスアカウントのメールアドレス
       paypal_email:     'suoaoshi@kimaroki.jp',
+      # 証明書ID
       paypal_cert_id:   'YYFQ434FHXAF6',
+      # PDT トークン
       paypal_pdt_token: 'Y3dcovaqgV_kjJV7I-rCjQlPETNfK66a8kLcvMW7AhqGTmoa_D0pMGEFQNe',
+
       path: {
+        # PayPal の公開鍵と証明書のパス
         paypal_cert:     "#{Rails.root}/config/paypal_cert_pem.txt",
+        # マーチャントの公開鍵と証明書
         merchant_cert:   "#{Rails.root}/config/paypal_my_public_key.pem",
+        # マーチャントの秘密鍵
         merchant_pubkey: "#{Rails.root}/config/paypal_my_private_key.pem",
       },
     }
